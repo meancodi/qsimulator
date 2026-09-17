4e68d653b5fa23db4b0979f60161ada3b1f0eb06
meancodi <srisayasandeepkarri@gmail.com>
2026-09-17 11:51:22 +0530

    Checkpoint 1 is done and tested

--- test_checkpoint_1.py
+++ test_checkpoint_1.py
@@ -6,21 +6,34 @@ import numpy as np
 
 
 def test_bell_state():
+
     sim = StateVectorSimulator(2)
+
     expected_initial = np.array([1,0,0,0],dtype=complex)
     print("Initial state = ", sim.state)
     assert np.allclose(sim.state, expected_initial) # checks if they are almost equal, within some tolerance
 
     sim.apply_single_qubit_gate(H,0)
-    expected_bell = np.array([1 / np.sqrt(2),0,0,1 / np.sqrt(2)],dtype=complex)
+    expected_after_h  = np.array(
+        [1/np.sqrt(2), 0, 1/np.sqrt(2), 0],
+        dtype=complex
+    )
     print("After H gate", sim.state)
-    assert np.allclose(sim.state, expected_bell)
+    assert np.allclose(sim.state, expected_after_h)
+
+    sim.apply_cnot(0, 1)
+    expected_bell = np.array(
+        [ 1 / np.sqrt(2), 0, 0, 1 / np.sqrt(2)],
+        dtype=complex
+    )
+    print("After CNOT =", sim.state)
+    assert np.allclose(sim.state,expected_bell)
 
     z0 = expectation_z(sim,0)
     z1 = expectation_z(sim,1)
     zz = expectation_zz(sim,0,1)
 
-    print(f"Z0 = {z0}\nZ1 = {z1}\nZZ - {zz}")
+    print(f"Z0 = {z0}\nZ1 = {z1}\nZZ = {zz}")
 
     assert np.isclose(z0, 0.0)
     assert np.isclose(z1, 0.0)
@@ -31,10 +44,16 @@ def test_bell_state():
 def test_measurement_bell_state():
     sim = StateVectorSimulator(2)
 
+    print(f"Initial state {sim.state}")
+    
     sim.apply_single_qubit_gate(H,0)
     sim.apply_cnot(0,1)
 
+    print(f"State after H and CNOT = {sim.state}")
+
     result = measure(sim, 0)
+
+    print(f"Result = {result}")
 
     assert result in [0,1]
 
@@ -49,7 +68,8 @@ def test_measurement_bell_state():
 
 
 
-
+print("---- bell state test ----")
 test_bell_state()
+print("---- measurement test ----")
 test_measurement_bell_state()
 
